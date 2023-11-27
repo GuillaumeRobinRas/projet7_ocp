709e371ff94ac6a0ef79f6e877325dc3b5ba67e7
Guillaume.ROBIN <guillaume.robin@ras-interim.fr>
2023-11-27 16:29:21 +0100

    modification pour les tests

--- API/env.py
+++ API/env.py
@@ -1,2 +1,2 @@
-lgb_model_path = 'model/lgb_classifier.pkl'
-df_path = '/home/guillaumerobin100/projet7_ocp/dataset/dataset.csv'
+lgb_model_path = '/home/guillaumerobin100/projet7_ocp/model/lgb_classifier.pkl'
+df_path = '/home/guillaumerobin100/projet7_ocp/dataset.csv'
